75cd30eff80777ad58b0865f6685ddb238263523
lhhernandez <hhernandez.isc@gmail.com>
2024-02-17 21:57:17 -0600

    End of chapter two

--- SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var game = Game()
+    @State var guess: RGB
+    @State var showScore = false
+    
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+      VStack {
+          CircleColor(rgb: game.target)
+          if !showScore {
+              Text("R: ??? G: ??? B: ???")
+                  .padding()
+          } else {
+              Text(game.target.intString())
+                  .padding()
+          }
+          CircleColor(rgb: guess)
+          Text(guess.intString())
+              .padding()
+          ColorSlider(value: $guess.red, trackColor: .red)
+          ColorSlider(value: $guess.green, trackColor: .green)
+          ColorSlider(value: $guess.blue, trackColor: .blue)
+          Button("Hit Me!") {
+              showScore = true
+              game.check(guess: guess)
+          }.alert(
+            isPresented: $showScore,
+            content: {
+              Alert(
+                title: Text("Your score"),
+                message: Text(String(game.scoreRound)),
+                dismissButton: .default(Text("Ok")) {
+                    game.startNewRound()
+                    guess = RGB()
+                })
+          })
+      }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+      ContentView(guess: RGB())
   }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var trackColor: Color
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .tint(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct CircleColor: View {
+    var rgb: RGB
+    var body: some View {
+        Circle()
+            .fill(Color(rgbStruct: rgb))
+    }
 }
